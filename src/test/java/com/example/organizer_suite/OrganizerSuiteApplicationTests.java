0753bf5d9b7ea3754b094ed25a1d093e7344884b
IngoTrautwein <it019@hdm-stuttgart.de>
2020-04-05 15:33:38 +0200

    real initial commit

---
+++ src/test/java/com/example/organizer_suite/OrganizerSuiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.organizer_suite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganizerSuiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
