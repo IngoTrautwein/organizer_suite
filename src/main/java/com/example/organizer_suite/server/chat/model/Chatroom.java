8cf5ed8b0e3295b2fac4a2060931488ea2020665
IngoTrautwein <it019@hdm-stuttgart.de>
2020-06-06 15:35:37 +0200

    Chatroom add members

--- src/main/java/com/example/organizer_suite/server/chat/model/Chatroom.java
+++ src/main/java/com/example/organizer_suite/server/chat/model/Chatroom.java
@@ -2,4 +2,20 @@ package com.example.organizer_suite.server.chat.model;
 
 public class Chatroom extends ChatModel {
 
+	private static final long serialVersionUID = 2581164132206892146L;
+	
+	private String topic = "";
+	
+	public Chatroom() {
+		super();
+		this.topic = "no topic set";
+	}
+	
+	public String getTopic() {
+		return this.topic;
+	}
+	
+	public void setTopic(String topic) {
+		this.topic = topic;
+	}
 }
