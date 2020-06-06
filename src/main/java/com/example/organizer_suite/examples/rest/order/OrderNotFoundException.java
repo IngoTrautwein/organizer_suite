a7f79e626148420ef8f7d5bbbc02bb228acb295d
IngoTrautwein <it019@hdm-stuttgart.de>
2020-06-06 15:07:18 +0200

    add BaseModel and Main class

--- src/main/java/com/example/organizer_suite/examples/rest/order/OrderNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.organizer_suite.examples.rest.order;
-
-public class OrderNotFoundException extends RuntimeException {
-	OrderNotFoundException(Long id) {
-		super("Could not find order " + id);
-	}
-}
--- src/main/java/com/example/organizer_suite/examples/rest/order/Status.java
+++
@@ -1,6 +0,0 @@
-package com.example.organizer_suite.examples.rest.order;
-
-enum Status {
-
-	IN_PROGRESS, COMPLETED, CANCELLED;
-}
