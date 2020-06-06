9619d90f28d3ed543328b13887145fdc310dc7bd
IngoTrautwein <it019@hdm-stuttgart.de>
2020-06-06 15:13:05 +0200

    CalendarModel extends BaseModel

--- src/main/java/com/example/organizer_suite/server/calendar/model/CalendarModel.java
+++ src/main/java/com/example/organizer_suite/server/calendar/model/CalendarModel.java
@@ -1,5 +1,9 @@
 package com.example.organizer_suite.server.calendar.model;
 
-public class CalendarModel {
+import com.example.organizer_suite.server.core.model.BaseModel;
+
+public class CalendarModel extends BaseModel {
+
+	private static final long serialVersionUID = 7924953889835418709L;
 
 }
