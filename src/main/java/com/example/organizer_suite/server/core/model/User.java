ea2bdeecbb5ffbb1691533c78719cbb85758224b
IngoTrautwein <it019@hdm-stuttgart.de>
2020-06-06 15:51:38 +0200

    add members to user class

--- src/main/java/com/example/organizer_suite/server/core/model/User.java
+++ src/main/java/com/example/organizer_suite/server/core/model/User.java
@@ -2,4 +2,28 @@ package com.example.organizer_suite.server.core.model;
 
 public class User extends BaseModel {
 
+	private static final long serialVersionUID = -4134034044082369218L;
+	
+	private String firstName = "";
+	private String surname = "";
+	
+	public User() {
+		super();
+	}
+	
+	public String getFirstName() {
+		return this.firstName;
+	}
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public String getSurname() {
+		return this.surname;
+	}
+	
+	public void setSurename(String surname) {
+		this.surname = surname;
+	}
 }
