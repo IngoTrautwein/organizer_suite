6bee8923ed1df547554033af4026c03938835f5a
IngoTrautwein <it019@hdm-stuttgart.de>
2020-06-06 15:45:50 +0200

    ContactList add members

--- src/main/java/com/example/organizer_suite/server/addressbook/model/ContactList.java
+++ src/main/java/com/example/organizer_suite/server/addressbook/model/ContactList.java
@@ -2,4 +2,20 @@ package com.example.organizer_suite.server.addressbook.model;
 
 public class ContactList extends AddressBookModel {
 
+	private static final long serialVersionUID = -2207244154426088416L;
+	
+	private String name = "";
+	
+	public ContactList() {
+		this.name = "empty contact list";
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 }
