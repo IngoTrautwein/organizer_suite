38bc6a4df6eebe1a6ee0f7bcb1233530c125d26b
IngoTrautwein <it019@hdm-stuttgart.de>
2020-06-06 15:44:15 +0200

    AddressBookModel add members

--- src/main/java/com/example/organizer_suite/server/addressbook/model/AddressBookModel.java
+++ src/main/java/com/example/organizer_suite/server/addressbook/model/AddressBookModel.java
@@ -1,5 +1,13 @@
 package com.example.organizer_suite.server.addressbook.model;
 
+import com.example.organizer_suite.server.core.model.BaseModel;
+
 public class AddressBookModel extends BaseModel {
 
+	private static final long serialVersionUID = -652426192549506841L;
+
+	public AddressBookModel() {
+		super();
+	}
+	
 }
