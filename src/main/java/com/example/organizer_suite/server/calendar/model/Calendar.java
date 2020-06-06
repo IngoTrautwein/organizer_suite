c30173f277379972d08b5d8122c9c77f5e2de3d9
IngoTrautwein <it019@hdm-stuttgart.de>
2020-06-06 15:13:56 +0200

    Calendar extending CalendarModel

--- src/main/java/com/example/organizer_suite/server/calendar/model/Calendar.java
+++ src/main/java/com/example/organizer_suite/server/calendar/model/Calendar.java
@@ -1,5 +1,7 @@
 package com.example.organizer_suite.server.calendar.model;
 
-public class Calendar {
+public class Calendar extends CalendarModel {
+
+	private static final long serialVersionUID = 8953739018131185228L;
 
 }
