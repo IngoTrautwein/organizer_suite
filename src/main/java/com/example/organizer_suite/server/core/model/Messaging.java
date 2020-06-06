4ba3ad41634b603b6f37c0be90816920f83c702c
IngoTrautwein <it019@hdm-stuttgart.de>
2020-06-06 15:53:31 +0200

    Messaging add members

--- src/main/java/com/example/organizer_suite/server/core/model/Messaging.java
+++ src/main/java/com/example/organizer_suite/server/core/model/Messaging.java
@@ -2,4 +2,21 @@ package com.example.organizer_suite.server.core.model;
 
 public class Messaging extends BaseModel {
 
+	private static final long serialVersionUID = -4735512250145370871L;
+	
+	private String text = "";
+	
+	public Messaging() {
+		super();
+		this.text = "empty message";
+	}
+	
+	public String getText() {
+		return this.text;
+	}
+	
+	public void setText(String text) {
+		this.text = text;
+	}
+	
 }
