8ee7fd858f9cf432f3be6e50b4868201f903ed29
IngoTrautwein <it019@hdm-stuttgart.de>
2020-06-11 15:04:27 +0200

    add member to SupplierModelFactory

--- src/main/java/com/example/organizer_suite/util/SupplierModelFactory.java
+++ src/main/java/com/example/organizer_suite/util/SupplierModelFactory.java
@@ -1,5 +1,16 @@
 package com.example.organizer_suite.util;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Supplier;
+
+import com.example.organizer_suite.server.core.model.BaseModel;
+
 public class SupplierModelFactory {
 
+	private static final Map<String, Supplier<? extends BaseModel>> rgisteredSuppliers = new HashMap<>();
+	
+	static {
+		
+	}
 }
