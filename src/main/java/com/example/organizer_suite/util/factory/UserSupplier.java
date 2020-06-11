30f23f856ef428f88ae5096be33f6e580cb78b9d
IngoTrautwein <it019@hdm-stuttgart.de>
2020-06-11 15:13:01 +0200

    UserSupplier realize get method

--- src/main/java/com/example/organizer_suite/util/factory/UserSupplier.java
+++ src/main/java/com/example/organizer_suite/util/factory/UserSupplier.java
@@ -3,7 +3,13 @@ package com.example.organizer_suite.util.factory;
 import java.util.function.Supplier;
 
 import com.example.organizer_suite.server.core.model.BaseModel;
+import com.example.organizer_suite.server.core.model.User;
 
 public class UserSupplier implements Supplier<BaseModel> {
+
+	@Override
+	public BaseModel get() {
+		return new User();
+	}
 
 }
